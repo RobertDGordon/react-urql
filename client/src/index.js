437329fa41530113ee1a8bbbc5bfb9b08c7c771a
Robert Gordon <Komeuppance@gmail.com>
2020-02-20 13:32:47 -0800

    Added URQL provider

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { createClient, Provider } from 'urql';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const client = createClient({
+    url: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn'
+  });
+
+ReactDOM.render(<Provider value={client}><App /></Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
