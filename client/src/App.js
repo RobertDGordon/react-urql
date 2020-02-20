f4894b0350556331d049a12a9fce12b721619936
Robert Gordon <Komeuppance@gmail.com>
2020-02-20 13:38:20 -0800

    Query success

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import './App.css';
 
+import { useQuery } from 'urql';
+
 function App() {
+  const [{fetching, data, error}] = useQuery({
+    query: `
+      {
+        allPosts(orderBy: createdAt_DESC, first: 10, skip: 0){
+          id
+          title
+          votes
+          url
+          createdAt
+        }
+      }
+    `
+  });
+
   return (
-      <div className="App">
-        <p>Yep.</p>
-      </div>
+    <pre>{JSON.stringify({fetching, data, error}, null, 2)}</pre>
   );
 }
 
